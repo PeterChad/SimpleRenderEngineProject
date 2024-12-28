4f8a33e0e2a72f91d4dd89e955781798bbfaa584
Peter Klarskov <peter.klarskov@hotmail.com>
2024-12-28 16:15:37 +0100

    Deleting more unused files

--- PlatformerProject/ExampleGame/Game/ScriptedComponent.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Engine/Component.h"
-
-class Component : public MyEngine::Component {
-	// TODO (challenge) implement one of the architecture for scripting described in class
-	// NB: may require changing the Engine, depending which one you want to implement
-};
